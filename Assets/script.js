936a6feefac9ca1743349762da57c4c3b3bbae16
Christoph551 <christophersimmonds551@gmail.com>
2022-12-28 16:32:12 -0700

    Creating save point. Need to address timing of window pop ups.

--- Assets/script.js
+++ Assets/script.js
@@ -6,28 +6,29 @@
 // Need password to be a maximum of 149 characters
 
 
-let generateBtn = document.querySelector("#generate"); // Moved generateBtn to group with other variables. Part of original source code
+const generateBtn = document.querySelector("#generate"); // Moved generateBtn to group with other variables. Part of original source code
 const numbers = [0,1,2,3,4,5,6,7,8,9] // Created constant for all numbers
 const lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"] // Created constant for all lowercase letters
 const upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"] // Created constant for all uppercase letters
 const specialCharacters = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", ";", ":", "'", ",", "<", ".", ">", "?"] // Created constant for all Special Characters.
+let passLength = 8;
 
-
+// Function for the initial alert window. Also included message with a return in the event the user enters something other than a number.
 function generatePassword() {
-  const buttonClick = window.prompt("How many characters would you like your password to contain?");
+  const userInput = window.prompt("How many characters would you like your password to contain?");
   const charLength = parseInt(userInput);
-
-  //prompt user
-  let passLength = prompt("Length?")
-  if (passLength < 8 || passLength >128) {
-    passAlert = alert("Password must be between 8 and 128 characters")
-  } else {
-    numberQuestion = confirm("Would you like to include numbers in your password?")
-    lowerQuestion = confirm("Would you like to include lower case letters in your password?")
-    upperQuestion = confirm("Would you like to include upper case letters in your password?")
-    specialQuestion = confirm("Would you like to include special characters in your password?")
+  if (isNaN(charLength)) {
+    window.alert("Please enter only numeric characters.");
+  } else (passLength < 8 || passLength > 128);
+  passAlert = window.alert("Password must be between 8 and 128 characters.")
+  return;
+}
+  if (passLength >= 8 && passLength <= 128) {
+    numberQuestion = window.prompt("Click 'OK' to include numbers in your password?")
+    lowerQuestion = window.prompt("Click 'OK' to include lower case letters in your password?")
+    upperQuestion = window.prompt("Click 'OK' to include upper case letters in your password?")
+    specialQuestion = window.prompt("Click 'OK' to include special characters in your password?")
   }
-} 
 
 // Write password to the #password input
 function writePassword() {
